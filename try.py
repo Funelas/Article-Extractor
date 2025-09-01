26272579e2c762a6bc6f0c53f0482057ce4ae836
Allan John Funelas <funelasallanjohn@gmail.com>
2025-09-01 15:37:40 +0800

    Bug fixes for headline bbox detection and annotation

--- try.py
+++ try.py
@@ -1,15 +1,15 @@
 from rapidfuzz import fuzz
 
-s1 = "k VP impeach trial set for Aug. 6"
-s2 = "VP impeach trial setfor _"
+s1 = "Govtset to roll"
+s2 = "Gov't set to roll out P20/k rice "
 
 res = fuzz.partial_ratio_alignment(s1, s2)
 print(fuzz.partial_token_sort_ratio(s1,s2))
 print(fuzz.ratio(s1, s2))            # 83
 print(res) # 100
-new_res = s1[res.src_start:res.src_end]
+new_res = s2[res.dest_start:res.dest_end]
 print(new_res)
-print(f"New String: {s1.replace(new_res, "")}")
+print(f"New String: {s2.replace(new_res, "")}")
 
 # first_list = list(s1)
 # second_list = list(s2)
